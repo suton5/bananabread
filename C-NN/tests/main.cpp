b619d9e58cd14c1d3ccde1427e91f596c6dfac0b
suton5 <sujay@dhcp-10-250-183-6.harvard.edu>
2020-04-22 19:33:36 -0400

    main.cpp push

--- C-NN/tests/main.cpp
+++ C-NN/tests/main.cpp
@@ -1,17 +1,25 @@
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
 #include <iostream>
-int main()
-{
-  Eigen::MatrixXd V(4,2);
-  V<<0,0,
-     1,0,
-     1,1,
-     0,1;
-  Eigen::MatrixXi F(2,3);
-  F<<0,1,2,
-     0,2,3;
-  Eigen::SparseMatrix<double> L;
-  std::cout<<"Hello, mesh: "<<std::endl<<L*V<<std::endl;
-  return 0;
+#include <assert.h>
+#include <unordered_map>
+#include <Eigen/Dense>
+#include <random>
+#include <chrono>
+#include <variant>
+#include <string>
+
+#include "cnn_math.hpp"
+#include "cnn.hpp"
+
+int main() {
+   std::unordered_map<std::string, arch_value_type> arch = {
+      {"num_hidden", 1},
+      {"width", 10},
+      {"input_dim", 1},
+      {"output_dim", 1},
+      {"activation_fn", ActivationFns::ReLU}
+   };
+   auto nn = FeedForward(arch);
+   Eigen::VectorXd x_train = Eigen::VectorXd::Random(5);
+   std::cout << nn.forward_pass(x_train) << std::endl;
+   return 0; 
 }

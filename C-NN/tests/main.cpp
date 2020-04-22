2ac727559976b126e0c7368424d18d9812ab02c2
Michael137 <michaelbuch12@gmail.com>
2020-04-22 19:25:30 -0400

    crazy makefile

---
+++ C-NN/tests/main.cpp
@@ -0,0 +1,17 @@
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <iostream>
+int main()
+{
+  Eigen::MatrixXd V(4,2);
+  V<<0,0,
+     1,0,
+     1,1,
+     0,1;
+  Eigen::MatrixXi F(2,3);
+  F<<0,1,2,
+     0,2,3;
+  Eigen::SparseMatrix<double> L;
+  std::cout<<"Hello, mesh: "<<std::endl<<L*V<<std::endl;
+  return 0;
+}

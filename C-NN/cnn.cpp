c0a218105dec13d49f0e9bbf1ca801bf44464d63
Michael137 <michaelbuch12@gmail.com>
2020-04-22 17:13:02 -0400

    fix errors

--- C-NN/cnn.cpp
+++ C-NN/cnn.cpp
@@ -1,50 +1,13 @@
-#include <iostream>
-#include <assert.h>
-#include <unordered_map>
+
 #include <Eigen/Dense>
-// #include <autodiff/forward.hpp>
 
-class FeedForward {
-	std::unordered_map architecture;
-	Eigen::ArrayXd weights;
-	Eigen::ArrayXd biases;
+#include "cnn.hpp"
 
-	FeedForward(std::unordered_map architecture, Eigen::ArrayXd weights = None, Eigen::ArrayXd biases = None) {
-		//activation function
-		self.h = architecture["activation_fn"];
-		//number of hidden layers (excludes input and output layers)
-		self.L = architecture["num_hidden"];
-		//width of hidden layers (assume constant for now)
-		self.width = architecture["width"];
-		//dimension of input layer
-		self.D_in = architecture["input_dim"];
-		//dimension of output layer
-		self.D_out = architecture["output_dim"];
-		//number of weights needed
-		self.D_W = (self.D_in * self.width) + ((self.L - 1) * self.width * self.width) + (self.width * self.D_out);
-		//number of biased needed
-		self.D_b = (self.width) + ((self.L - 1) * self.width) + (self.D_out);
-		//initialise weights
-		if (weights == None) {
-			self.W = random(self.D_W);
-		} else {
-			self.W = weights;
-		}
-		//initialise biases
-		if (biases == None) {
-			self.b = random(self.D_b);
-		} else {
-			self.b = biases;
-		}
-	}
-
-	Eigen::ArrayXd forward_pass(x) {
-		assert (self.W.size() == self.D_W);
-		assert (self.b.size() == self.D_b);  
-	//   assert (shape());
-		W = self.W(Eigen::seq(0, self.width * self.D_in - 1));
-		b = self.b(Eigen::seq(0, self.width - 1));
-		
-		return x;
-	}
+Eigen::ArrayXd forward_pass(Eigen::ArrayXd x) {
+	assert (W.size() == self.D_W);
+	assert (b.size() == self.D_b);  
+	W = W(Eigen::seq(0, width * D_in - 1));
+	b = b(Eigen::seq(0, width - 1));
+	
+	return x;
 }
--- C-NN/cnn.hpp
+++ C-NN/cnn.hpp
@@ -1,1 +1,47 @@
-class FeedForward;
+#ifndef CNN_H_IN
+#define CNN_H_IN
+
+#include <iostream>
+#include <assert.h>
+#include <unordered_map>
+#include <Eigen/Dense>
+
+
+
+class FeedForward {
+  private:
+	std::unordered_map architecture;
+	std::function<Eigen::ArrayXd(Eigen::ArrayXd)> h;
+	int L;
+	int width;
+	int D_in;
+	int D_out;
+	int D_W;
+	int D_b;
+	Eigen::ArrayXd weights;
+	Eigen::ArrayXd biases;
+
+  public:
+	FeedForward(std::unordered_map architecture) 
+		: h(architecture["activation_fn"]),
+		  L(architecture["num_hidden"]),
+		  //width of hidden layers (assume constant for now)
+		  width(architecture["width"]),
+		//dimension of input layer
+		  D_in(architecture["input_dim"]),
+		//dimension of output layer
+		  D_out(architecture["output_dim"]),
+		//number of weights needed
+		  D_W((D_in * width) + ((L - 1) * width * width) + (width * D_out)),
+		//number of biased needed
+		  D_b((width) + ((L - 1) * width) + (D_out)),
+		//initialise weights
+		  weights(random(D_W)),
+		//initialise biases
+		  biases(random(D_b))
+	{}
+
+	Eigen::ArrayXd forward_pass(Eigen::ArrayXd x);
+}
+
+#endif // CNN_H_IN

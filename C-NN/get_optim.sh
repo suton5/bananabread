d4b2340cdeddc41ce49f8e09e251ffa6b41b825e
Michael137 <michaelbuch12@gmail.com>
2020-04-25 02:17:57 -0400

    fix optim build

--- C-NN/get_optim.sh
+++ C-NN/get_optim.sh
@@ -5,15 +5,16 @@ if [ ! -d external/include/optim ]; then
 
 	mkdir external/include/optim
 	cd optim
-	./configure --header-version-only
-	mv header_version_only ../external/include/optim
+	./configure -i $PWD/../external
+	make -j4
+	make install
 
 	wget http://sourceforge.net/projects/arma/files/armadillo-9.860.2.tar.xz
 	tar xf armadillo-9.860.2.tar.xz
 	cd armadillo-9.860.2
-	cmake . -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../external
-	make all
-	make install
+	cmake . -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../../external
+	make -j4 all
+	make -j4 install
 
 	cd ../..
 	rm -rf optim
